c96bb455293ce8144702267f3a137e12d2b5958a
DeyvisDev <97794872+DeyvisDev@users.noreply.github.com>
2023-09-12 23:27:42 -0400

    initial imc app

---
+++ app/src/main/java/com/example/menuappprueba/IMCCalculator/ImcCalculatorActivity.kt
@@ -0,0 +1,15 @@
+package com.example.menuappprueba.IMCCalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.menuappprueba.R
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc_calculator)
+
+
+
+    }
+}
--- app/src/main/java/com/example/menuappprueba/MenuActivityV2.kt
+++ app/src/main/java/com/example/menuappprueba/MenuActivityV2.kt
@@ -1,11 +1,32 @@
 package com.example.menuappprueba
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+import com.example.menuappprueba.IMCCalculator.ImcCalculatorActivity
 
 class MenuActivityV2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_v2)
+
+        val btnSaludar = findViewById<AppCompatButton>(R.id.btnSaludarApp)
+        btnSaludar.setOnClickListener{
+            navigateToSaludarApp()
+        }
+
+        val btnIMC = findViewById<AppCompatButton>(R.id.btnIMCApp)
+        btnIMC.setOnClickListener{
+            navigateIMCApp()
+        }
+    }
+    private fun navigateToSaludarApp(){
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
+    private fun navigateIMCApp(){
+        val intent = Intent(this,ImcCalculatorActivity::class.java)
+        startActivity(intent)
     }
 }
