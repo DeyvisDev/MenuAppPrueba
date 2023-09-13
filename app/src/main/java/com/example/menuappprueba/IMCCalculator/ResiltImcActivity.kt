f96976e9f3bfc7065b152cde8b93bae7d0f94511
DeyvisDev <97794872+DeyvisDev@users.noreply.github.com>
2023-09-13 19:22:15 -0400

    IMC completo version 1, fallo en seleccionar hombre o mujer, n o se queda seleccionado

--- app/src/main/java/com/example/menuappprueba/IMCCalculator/ResiltImcActivity.kt
+++ app/src/main/java/com/example/menuappprueba/IMCCalculator/ResiltImcActivity.kt
@@ -2,7 +2,9 @@ package com.example.menuappprueba.IMCCalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.menuappprueba.IMCCalculator.ImcCalculatorActivity.Companion.IMC_KEY
 import com.example.menuappprueba.R
 
@@ -10,6 +12,7 @@ class ResiltImcActivity : AppCompatActivity() {
     private lateinit var tvResoult:TextView
     private lateinit var tvIMC:TextView
     private lateinit var tvDescription:TextView
+    private lateinit var btnRecalculate:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resilt_imc)
@@ -18,27 +21,38 @@ class ResiltImcActivity : AppCompatActivity() {
 
         initComponent()
         initUI(imc)
+        iniListener()
+    }
+
+    private fun iniListener() {
+        btnRecalculate.setOnClickListener {
+            onBackPressed()
+        }
     }
 
     private fun initUI(imc: Double) {
         tvIMC.text = imc.toString()
         when(imc){
             in 0.00 .. 18.50 -> {//bajo peso
-                tvResoult
-                tvDescription
+                tvResoult.text = getString(R.string.title_bajo_peso)
+                tvResoult.setTextColor(ContextCompat.getColor(this,R.color.peso_bajo))
+                tvDescription.text = getString(R.string.description_bajo_peso)
 
             }
             in 18.51 .. 24.99 -> {//peso normal
-                tvResoult
-                tvDescription
+                tvResoult.text = getString(R.string.title_normal)
+                tvResoult.setTextColor(ContextCompat.getColor(this,R.color.normal))
+                tvDescription.text = getString(R.string.description_normal)
             }
             in 25.00 .. 29.99 -> {//sobre peso
-                tvResoult
-                tvDescription
+                tvResoult.text = getString(R.string.title_sobre_peso)
+                tvResoult.setTextColor(ContextCompat.getColor(this,R.color.sobre_peso))
+                tvDescription.text = getString(R.string.description_sobre_peso)
             }
             in 30.00 .. 99.00 -> {//obesidad
-                tvResoult
-                tvDescription
+                tvResoult.text = getString(R.string.title_obecidad)
+                tvResoult.setTextColor(ContextCompat.getColor(this,R.color.obecidad))
+                tvDescription.text = getString(R.string.description_obecidad)
             }
             else ->{//error
                 tvIMC.text = getString(R.string.error)
@@ -52,6 +66,7 @@ class ResiltImcActivity : AppCompatActivity() {
         tvIMC = findViewById(R.id.tvIMC)
         tvDescription = findViewById(R.id.tvDescription)
         tvResoult = findViewById(R.id.tvResoult)
+        btnRecalculate = findViewById(R.id.btnRecalculate)
 
     }
 }
