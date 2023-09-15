0b91fadf4eb566e0fe27ac73e4c7df22e43e7016
DeyvisDev <97794872+DeyvisDev@users.noreply.github.com>
2023-09-14 23:18:57 -0400

    inicio de reciclerview

--- app/src/main/java/com/example/menuappprueba/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/menuappprueba/todoapp/TodoActivity.kt
@@ -8,5 +8,8 @@ class TodoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
+
+
+
     }
 }
