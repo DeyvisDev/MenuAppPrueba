eef03155382d9ad0b9eb9217f8c38922de4e2250
DeyvisDev <97794872+DeyvisDev@users.noreply.github.com>
2023-09-14 21:54:05 -0400

    Creacion de App todo

--- app/src/main/java/com/example/menuappprueba/MenuActivityV2.kt
+++ app/src/main/java/com/example/menuappprueba/MenuActivityV2.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
 import com.example.menuappprueba.IMCCalculator.ImcCalculatorActivity
+import com.example.menuappprueba.Recycler.RecyclerActivity
+import com.example.menuappprueba.todoapp.TodoActivity
 
 class MenuActivityV2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,13 +22,34 @@ class MenuActivityV2 : AppCompatActivity() {
         btnIMC.setOnClickListener{
             navigateIMCApp()
         }
+
+        val btnGoReciclerView = findViewById<AppCompatButton>(R.id.btnGotReciclerView)
+        btnGoReciclerView.setOnClickListener{
+            navidateToReciclerView()
+        }
+
+        val btnTodoApp = findViewById<AppCompatButton>(R.id.btnTodoApp)
+        btnTodoApp.setOnClickListener{
+            navidatebtnTodoApp()
+        }
     }
+
+    private fun navidatebtnTodoApp() {
+        val intent = Intent(this,TodoActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navidateToReciclerView() {
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
+
     private fun navigateToSaludarApp(){
         val intent = Intent(this,MainActivity::class.java)
         startActivity(intent)
     }
     private fun navigateIMCApp(){
-        val intent = Intent(this,ImcCalculatorActivity::class.java)
+        val intent = Intent(this,RecyclerActivity::class.java)
         startActivity(intent)
     }
 }
