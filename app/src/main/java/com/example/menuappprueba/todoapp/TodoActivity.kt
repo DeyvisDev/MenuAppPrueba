de1c10bc64da1610b2101e396b84d782d3722cab
DeyvisDev <97794872+DeyvisDev@users.noreply.github.com>
2023-09-15 20:14:00 -0400

    task category

--- app/src/main/java/com/example/menuappprueba/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/menuappprueba/todoapp/TodoActivity.kt
@@ -8,6 +8,9 @@ class TodoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
+        "Busines"
+        "Personal"
+        "Others"
 
 
 
