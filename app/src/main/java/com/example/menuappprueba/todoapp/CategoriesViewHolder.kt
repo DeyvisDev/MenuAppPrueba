c9b251df895f5570f8419b7765eb76ed7ace7a0a
DeyvisDev <97794872+DeyvisDev@users.noreply.github.com>
2023-09-16 23:59:46 -0400

    adapter for categories complete

--- app/src/main/java/com/example/menuappprueba/todoapp/CategoriesViewHolder.kt
+++ app/src/main/java/com/example/menuappprueba/todoapp/CategoriesViewHolder.kt
@@ -7,10 +7,17 @@ import com.example.menuappprueba.R
 
 class CategoriesViewHolder(view: View):RecyclerView.ViewHolder(view) {
     private val tvCategoryName:TextView = view.findViewById(R.id.tvCategoryName)
-    private val vDivider:TextView = view.findViewById(R.id.vDivider)
+    private val vDivider:View = view.findViewById(R.id.vDivider)
 
     fun render(taskCategory: TaskCategory){
-
+        tvCategoryName.text = "Ejemplo"
+        when(taskCategory){
+            TaskCategory.Business -> {
+                tvCategoryName.text = "Negoco"
+            }
+            TaskCategory.Other -> TODO()
+            TaskCategory.Personal -> TODO()
+        }
     }
 
 }
--- app/src/main/java/com/example/menuappprueba/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/menuappprueba/todoapp/TodoActivity.kt
@@ -2,29 +2,34 @@ package com.example.menuappprueba.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.menuappprueba.R
 import com.example.menuappprueba.Recycler.RecyclerActivity
 
 class TodoActivity : AppCompatActivity() {
 
+    private val categries = listOf(
+        TaskCategory.Other,
+        TaskCategory.Personal,
+        TaskCategory.Business
+    )
+
     private lateinit var rvCategories:RecyclerView
+    private lateinit var categoriesAdapter: CategoriesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
         initComponent()
         initUI()
-
-
-
     }
-
-
-
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
     }
     private fun initUI() {
-
+        categoriesAdapter = CategoriesAdapter(categries)
+        rvCategories.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        rvCategories.adapter = categoriesAdapter
     }
 }
