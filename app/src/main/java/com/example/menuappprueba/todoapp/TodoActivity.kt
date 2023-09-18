e4cc671be0d604434671cb61900d15ee1d946c93
DeyvisDev <97794872+DeyvisDev@users.noreply.github.com>
2023-09-17 23:47:36 -0400

    CREATE taskadapter taskviewholder and Task class

--- app/src/main/java/com/example/menuappprueba/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/menuappprueba/todoapp/TodoActivity.kt
@@ -19,6 +19,8 @@ class TodoActivity : AppCompatActivity() {
     private lateinit var rvCategories:RecyclerView
     private lateinit var categoriesAdapter: CategoriesAdapter
 
+    private lateinit var rvTasks:RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
@@ -27,6 +29,7 @@ class TodoActivity : AppCompatActivity() {
     }
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
+        rvTasks = findViewById(R.id.rvTasks)
     }
     private fun initUI() {
         categoriesAdapter = CategoriesAdapter(categries)
