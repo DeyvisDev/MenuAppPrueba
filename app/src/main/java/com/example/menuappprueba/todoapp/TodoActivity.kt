442a6ebee5d91329dc5e85625e4584395875023d
DeyvisDev <97794872+DeyvisDev@users.noreply.github.com>
2023-09-19 00:44:18 -0400

    conecction dialog with vist, radius group, etittext and button

--- app/src/main/java/com/example/menuappprueba/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/menuappprueba/todoapp/TodoActivity.kt
@@ -3,6 +3,10 @@ package com.example.menuappprueba.todoapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.menuappprueba.R
@@ -48,6 +52,20 @@ class TodoActivity : AppCompatActivity() {
     private fun showDialog(){
         val dialog = Dialog(this)
         dialog.setContentView(R.layout.dialog_task)
+        val btnAddTask:Button = dialog.findViewById(R.id.btnAddTask)
+        val etTask:EditText = dialog.findViewById(R.id.etTask)
+        val rgCategories:RadioGroup = dialog.findViewById(R.id.rgCategories)
+        btnAddTask.setOnClickListener {
+            val selectedId = rgCategories.checkedRadioButtonId
+            val selectedRadioButton:RadioButton = rgCategories.findViewById(selectedId)
+            val currentCategory:TaskCategory = when(selectedRadioButton.text){
+                "Negocios" -> TaskCategory.Business
+                "Personal" -> TaskCategory.Personal
+                else -> TaskCategory.Other
+            }
+            tasks.add(Task(etTask.text.toString(),currentCategory))
+            dialog.hide()
+        }
         dialog.show()
     }
 
