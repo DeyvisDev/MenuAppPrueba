b5e91397c47b30a1801314cdc0e44f0b56145501
DeyvisDev <97794872+DeyvisDev@users.noreply.github.com>
2023-09-21 16:22:54 -0400

    add image to imageview from picasso

--- app/src/main/java/com/example/menuappprueba/superheroapp/SuperHeroViewHolder.kt
+++ app/src/main/java/com/example/menuappprueba/superheroapp/SuperHeroViewHolder.kt
@@ -3,11 +3,14 @@ package com.example.menuappprueba.superheroapp
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.menuappprueba.databinding.ItemSuperheroBinding
+import com.squareup.picasso.Picasso
 
 class SuperHeroViewHolder(view: View):RecyclerView.ViewHolder(view) {
     private val binding = ItemSuperheroBinding.bind(view)
     fun bind(superheroItemResponse: SuperheroItemResponse){
         binding.tvDuperheroName.text = superheroItemResponse.name
-        binding.ivSuperhero
+
+
+        Picasso.get().load(superheroItemResponse.SuperherImage.url).into(binding.ivSuperhero)
     }
 }
