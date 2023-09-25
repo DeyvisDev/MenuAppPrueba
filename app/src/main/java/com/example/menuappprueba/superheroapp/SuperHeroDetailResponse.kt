b1e9e8914f4b83887b553ad1db588a0845d7e949
DeyvisDev <97794872+DeyvisDev@users.noreply.github.com>
2023-09-24 23:31:58 -0400

    superHeroDetails with id complete

--- app/src/main/java/com/example/menuappprueba/superheroapp/SuperHeroDetailResponse.kt
+++ app/src/main/java/com/example/menuappprueba/superheroapp/SuperHeroDetailResponse.kt
@@ -1,3 +1,16 @@
 package com.example.menuappprueba.superheroapp
 
-data class SuperHeroDetailResponse
+import com.google.gson.annotations.SerializedName
+
+data class SuperHeroDetailResponse(
+    @SerializedName("name") val name:String,
+    @SerializedName("powerstats") val powerstats:powerStatsResponse
+)
+data class powerStatsResponse(
+    @SerializedName("intelligence") val intelligence:String,
+    @SerializedName("strength") val strength:String,
+    @SerializedName("speed") val speed:String,
+    @SerializedName("durability") val durability:String,
+    @SerializedName("power") val power:String,
+    @SerializedName("combat") val combat:String
+    )
