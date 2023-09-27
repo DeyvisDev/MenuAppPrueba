dd0cf5fd175a932a2a49d937438b0b74384f4750
DeyvisDev <97794872+DeyvisDev@users.noreply.github.com>
2023-09-26 22:08:46 -0400

    store data from volume

--- app/src/main/java/com/example/menuappprueba/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/menuappprueba/settings/SettingsActivity.kt
@@ -1,17 +1,50 @@
 package com.example.menuappprueba.settings
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import com.example.menuappprueba.R
 import com.example.menuappprueba.databinding.ActivitySettingsBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name="settings")
 
 class SettingsActivity : AppCompatActivity() {
+    companion object{
+        const val VOLUME_LVL = "volume_lvl"
+    }
     private lateinit var binding:ActivitySettingsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
 
+
+    }
+
+    private fun initUI() {
+        binding.rsVolume.addOnChangeListener { _, value, _ ->
+            Log.i("DeyvisDev","El valor es $value")
+            CoroutineScope(Dispatchers.IO).launch {
+                saveVolume(value.toInt())
+            }
+
+        }
+    }
+
+    private suspend fun saveVolume(value:Int){
+        dataStore.edit { preferences ->
+            preferences[intPreferencesKey(VOLUME_LVL)] = value
+        }
 
     }
 }
